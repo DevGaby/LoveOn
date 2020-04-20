c25c2686aad7db213cbed97fbc5c6a95104315f7
DevGaby <sgabrielcalixte@gmail.com>
2020-04-20 16:42:03 +0200

    Footer
    Create footer

--- love-on/src/components/footer/footer.js
+++ love-on/src/components/footer/footer.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 
 export default class Footer extends Component {
     render(){
-        return <footer> FOOTER </footer>
+        return (
+        <footer className="blockquote-footer text-center">
+        <p className="text-muted">
+          2020@ALJASUTO |<a href="mailto:alsajuto@contact.com"> alsajuto@contact.com</a>
+           {/* <cite title="Source Title">Source Title</cite> */}
+        </p>
+      </footer>
+        );
     }
 }
--- love-on/src/components/index.js
+++ love-on/src/components/index.js
@@ -1,7 +1,2 @@
 export { default as Header } from './header/header';
-export { default as Description } from './description/description';
-export { default as Coaching } from './coaching/coaching';
-export { default as ListStories } from './stories/list-stories';
-export { default as Contact } from './contact/contact';
-export { default as Download } from './download/download';
 export { default as Footer } from './footer/footer';
